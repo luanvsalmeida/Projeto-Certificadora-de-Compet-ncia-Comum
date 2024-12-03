5b8d7fcfead9057db9bdc8351d5c6dad2da50aeb
drezix <andre.t.agostinis@gmail.com>
2024-12-03 20:20:55 -0300

    feat: Criando diretórios para depois inserir os arquivos com as otimizações, testes, comentários e etc.

---
+++ Otimizações/main.c
@@ -0,0 +1,4 @@
+#include "Algoritmos/algoritmo.h"
+#include "Estruturas de Controle/estruturaDeControle.h"
+#include "Operadores e Tipos de Dados/operadoresDados.h"
+#include "Vetores e Matrizes/vetorMatriz.h"
