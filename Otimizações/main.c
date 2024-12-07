d7d37b95a1f635685bdc4f33e961124038a70ab0
drezix <andre.t.agostinis@gmail.com>
2024-12-07 15:47:23 -0300

    feat: adicionando main e executável do programa

--- Otimizações/main.c
+++ Otimizações/main.c
@@ -2,3 +2,45 @@
 #include "Estruturas de Controle/estruturaDeControle.h"
 #include "Operadores e Tipos de Dados/operadoresDados.h"
 #include "Vetores e Matrizes/vetorMatriz.h"
+#include <stdio.h>
+
+int main() {
+    int escolha;
+
+    do {
+        printf("\nMenu:\n");
+        printf("1. Conversor de Altura (Algoritmos)\n");
+        printf("2. Média de Pontos (Estruturas de Controle)\n");
+        printf("3. Percentual de Acertos (Estruturas de Controle)\n");
+        printf("4. Estatísticas de Jogadores (Vetores/Matrizes)\n");
+        printf("5. Pontos por Jogo (Vetores/Matrizes)\n");
+        printf("0. Sair\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &escolha);
+
+        switch (escolha) {
+            case 1:
+                conversorAltura();
+                break;
+            case 2:
+                mediaPontos();
+                break;
+            case 3:
+                percentualAcertos();
+                break;
+            case 4:
+                estatisticasJogadores();
+                break;
+            case 5:
+                pontosJogos();
+                break;
+            case 0:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida.\n");
+        }
+    } while (escolha != 0);
+
+    return 0;
+}
