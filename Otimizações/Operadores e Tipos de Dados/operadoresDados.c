39d882be7b1c8ed487e5c67499367eb9d2d7f586
drezix <andre.t.agostinis@gmail.com>
2024-12-07 15:44:57 -0300

    feat: adicionando Operadores e Tipos de Dados

---
+++ Otimizações/Operadores e Tipos de Dados/operadoresDados.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void conversorAltura();
+void mediaPontos();
+void percentualAcertos();
+void estatisticasJogadores();
+void pontosJogos();
---
+++ Otimizações/Operadores e Tipos de Dados/operadoresDados.h
@@ -0,0 +1,10 @@
+#ifndef OPERADORESDADOS_H
+#define OPERADORESDADOS_H
+
+void conversorAltura();
+void mediaPontos();
+void percentualAcertos();
+void estatisticasJogadores();
+void pontosJogos();
+
+#endif // OPERADORESDADOS_H
